635ba5005d92c22cb414a1c638e27a65bb83d983
lilHerbie <ht.alting@student.avans.nl>
2024-02-21 14:26:17 +0100

    create base layout
    I have created a base layout file that can be extended so that standard functionality like tailwind and preline does not need to be added to each blade file separately.

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<head>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    @yield('content');
+</body>
+
+</html>
