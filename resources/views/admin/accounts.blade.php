7a4bbd72d5b9705eaa33300744481fe9d2c17ebe
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-02-25 15:08:51 +0100

    feat(accounts): replaced strings with localization
    All hardcoded strings are replaced with localization strings, except the email (due to it being an example).

--- resources/views/admin/accounts.blade.php
+++ resources/views/admin/accounts.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.base')
 
-@section('title', 'TODO: put title here')
+@section('title', __('accounts.title'))
 
 @section('content')
+{{--TODO: Add navbar --}}
 
 <div class="mx-auto text-center">
-    <h1 class="text-4xl m-8">Account management</h1>
+    <h1 class="text-4xl m-8">{{__('accounts.page_title')}}</h1>
 
     <div class="w-1/2 mx-auto overflow-hidden border rounded-lg">
         <table class="w-full divide-y divide-gray-200">
             <thead>
                 <tr>
-                    <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Email</th>
-                    <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Role</th>
+                    <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">{{__('accounts.email')}}</th>
+                    <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">{{__('accounts.role')}}</th>
                 </tr>
             </thead>
             <tbody class="divide-y divide-gray-200">
@@ -24,9 +25,9 @@
                         <div class="relative inline-block text-left">
                             <select id="dropdownRoles" name="dropdownRoles"
                                 class="block appearance-none w-full border border-gray-300 py-2 px-4 pr-8 rounded leading tight focus:outline-none focus:bg-white focus:border-gray-500">
-                                <option value="gebruiker">Gebruiker</option>
-                                <option value="teamleider">Teamleider</option>
-                                <option value="admin">Admin</option>
+                                <option value="gebruiker">{{__('accounts.user')}}</option>
+                                <option value="teamleider">{{__('accounts.team_leader')}}</option>
+                                <option value="admin">{{__('accounts.admin')}}</option>
                             </select>
                         </div>
                     </td>
@@ -36,7 +37,7 @@
     </div>
 
     <div class="fixed bottom-0 right-0 m-4">
-        <button id="saveBtn" name="saveBtn" class="bg-blue-500 text-white px-8 py-4 rounded">Opslaan</button>
+        <button id="saveBtn" name="saveBtn" class="bg-blue-500 text-white px-8 py-4 rounded">{{__('accounts.save_button')}}</button>
     </div>
 </div>
 
