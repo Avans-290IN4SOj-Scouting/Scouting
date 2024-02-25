2941639f31c6948c9faa5a9904926893b7db9854
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-02-25 15:05:03 +0100

    feat(localization): add Dutch localization SOJ-91
    Added a localization file for the accounts view. Title is commented out as it is currently unknown.

---
+++ lang/nl/accounts.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    //"title"=> "",
+    "page_title"=> "Account management",
+    "email"=> "Emailadres",
+    "role"=> "Rol",
+    "user"=> "gebruiker",
+    "team_leader"=> "teamleider",
+    "admin"=> "admin",
+    "save_button"=> "Opslaan",
+];
