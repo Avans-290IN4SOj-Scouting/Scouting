aeecc291261175f98c16b6d79cf50a7cc93f85e5
lilHerbie <ht.alting@student.avans.nl>
2024-02-26 11:26:13 +0100

    Commit to merge
    This is not a meaningfull commit, i just want to pull content from main to my branch.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,13 +2,6 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
-
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
