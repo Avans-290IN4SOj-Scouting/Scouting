243217b07a1221ff5205f61572baa0995ab660aa
JrnJ <jeroenjochems@outlook.com>
2024-02-26 13:10:33 +0100

    fix(ui): ui fixes for light theme

--- resources/views/orders/overview.blade.php
+++ resources/views/orders/overview.blade.php
@@ -53,11 +53,11 @@
                                                     <div class="product-shopping-cart">
                                                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                             <path d="M8 22C8.55228 22 9 21.5523 9 21C9 20.4477 8.55228 20 8 20C7.44772 20 7 20.4477 7 21C7 21.5523 7.44772 22 8 22Z"
-                                                                stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                                                                stroke="#2563eb" stroke-linecap="round" stroke-linejoin="round"/>
                                                             <path d="M19 22C19.5523 22 20 21.5523 20 21C20 20.4477 19.5523 20 19 20C18.4477 20 18 20.4477 18 21C18 21.5523 18.4477 22 19 22Z"
-                                                                stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                                                                stroke="#2563eb" stroke-linecap="round" stroke-linejoin="round"/>
                                                             <path d="M2.05005 2.05005H4.05005L6.71005 14.47C6.80763 14.9249 7.06072 15.3315 7.42576 15.6199C7.7908 15.9083 8.24495 16.0604 8.71005 16.05H18.49C18.9452 16.0493 19.3865 15.8933 19.7411 15.6079C20.0956 15.3224 20.3422 14.9246 20.4401 14.48L22.09 7.05005H5.12005"
-                                                                stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                                                                stroke="#2563eb" stroke-linecap="round" stroke-linejoin="round"/>
                                                         </svg>
                                                     </div>
                                                 </button>
