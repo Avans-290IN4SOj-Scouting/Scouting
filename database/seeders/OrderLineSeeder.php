a8ee661ec38792cc906dda3d317ebd0a4de71c5c
JrnJ <jeroenjochems@outlook.com>
2024-02-28 12:55:23 +0100

    feat(db): tables without content

---
+++ database/seeders/OrderLineSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OrderLineSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
