9cd1e5d7cfea06f654d007c40b33b02f38dfc9b3
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-03-01 18:56:50 +0100

    fix(roles/perms): fixed issue with role assignment
    Previously, when roles were to be assigned, an exception would be thrown regarding the missing table named 'role_user'. This table should have been generated automatically, but wasn't. The problem is fixed by manually creating a migration containing this table.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(RoleSeeder::class);
         $this->call(PermissionSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
