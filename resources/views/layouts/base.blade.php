3800ac3082cfe5addbe5e680a88e3e95ea30b0b5
JrnJ <jeroenjochems@outlook.com>
2024-03-01 17:25:44 +0100

    update merge request

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -12,7 +12,7 @@
     @stack('scripts')
 </head>
 
-<body class="bg-white dark:bg-black antialiased">
+<body class="bg-white dark:bg-slate-900">
 <x-navbar></x-navbar>
 
 <div style="padding-top: 70px">
