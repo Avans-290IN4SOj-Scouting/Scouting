eb1ca6ba9ace613f996ad6d5a9772caa72fb1af5
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-03-02 20:23:33 +0100

    fix(roles/perms): fixed issue with role assignment (#17)
    Previously, when roles were to be assigned, an exception would be thrown regarding the missing table named 'role_user'. This table should have been generated automatically, but wasn't. The problem is fixed by manually creating a migration containing this table.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasRoles, HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
