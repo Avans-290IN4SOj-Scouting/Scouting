674bc6eda06e70209bdadd733c653dcdc75e1883
Linde Rus <lsy.rus@student.avans.nl>
2024-03-06 21:28:18 +0100

    refactor(dusk): remove unnecessary classes

--- resources/views/confirmPassword.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
--- resources/views/resetPassword.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
-</div>
