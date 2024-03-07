730b3c2000313f1b9159b36cf371755748f721f3
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-03-07 16:15:43 +0100

    feat(toast): toast disappears after 3 sec
    Previously, the toast didn't disappear after appearing for the first time. With a JS script, the opacity of the toast is set; opacity is used for a vanishing animation in the future.

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,6 +1,11 @@
+<head>
+    <title></title>
+    @vite(['resources/js/toast.js'])
+</head>
+
 <div class="absolute bottom-0 end-0 m-6"
      role="alert">
-    <div class="toast-{{ $type }} flex p-4">
+    <div id="toast" class="toast-{{ $type }} flex p-4">
         <span class="font-bold">
             {{ __('toast/toast_types.' . $type) }} -
         </span>
