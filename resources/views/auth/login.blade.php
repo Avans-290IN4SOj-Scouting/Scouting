dd29fe6d379ccf1de4ca1675271c9ca670db0c55
Linde Rus <lsy.rus@student.avans.nl>
2024-03-08 11:17:48 +0100

    fix(frontend-login-registration): error message and remembering email

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,7 +30,8 @@
                                 <div class="relative">
                                     <input type="email" id="email" name="email"
                                            class="py-3 px-4 block w-full border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
-                                           required>
+                                           required
+                                           value="{{old('email')}}">
                                     @error('email')
                                         <span class="text-red-500">{{ $message }}</span>
                                     @enderror
