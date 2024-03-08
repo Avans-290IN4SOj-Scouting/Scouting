e7e5c32bdee4965f8b8410a624ed8fc52772c58f
JrnJ <jeroenjochems@outlook.com>
2024-03-08 15:32:10 +0100

    feat(route localization): description
     added route localization to comply with test

---
+++ lang/nl/route.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'overview' => '/product-categorie',
+    'shopping-cart' => '/winkelwagen',
+    'product' => '/product',
+    'checkout' => '/bestelling',
+    'completed-order' => '/bestelling-voltooid'
+];
--- routes/web.php
+++ routes/web.php
@@ -29,10 +29,12 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::get(__('navbar.cart'), function () {
+    dd("help");
     return view('customer.cart');
 })->name('cart');
 
 Route::get(__('navbar.checkout'), function () {
+    dd("help");
     return view('customer.checkout');
 })->name('checkout');
 
@@ -46,39 +48,6 @@ Route::middleware('role:admin')->group(function () {
     })->name('manage-products');
 });
 
-// TODO: Add a route for the login page
-Route::get(__('navbar.login'), function () {
-    return view('customer.home');
-}) -> name('login');
-
-// Orders
-Route::get('/product-categorie/{category?}/{size?}', [OrderController::class, 'overview'])
-    ->name('orders.overview')
-    ->defaults('category', 'bevers')
-    ->defaults('size', 'S');
-
-Route::get('/product/{id}/{size?}', [OrderController::class, 'product'])
-    ->name('orders.product')
-    ->defaults('size', 'S');
-Route::get('/products', function() {
-    return view('orders.overview');
-});
-
-Route::get('/bestelling', [OrderController::class, 'order'])
-    ->name('orders.order');
-Route::post('/complete-order', [OrderController::class, 'completeOrder'])
-    ->name('orders.complete-order');
-Route::get('/bestelling-voltooid', [OrderController::class, 'completedOrder'])
-    ->name('orders.completed');
-
-Route::get('/winkelwagen', [ShoppingCartController::class, 'index'])
-    ->name('shoppingcart.index');
-
-// Shopping Cart
-Route::post('/shoppingcart/insert/{id}', [ShoppingCartController::class, 'insert'])
-    ->name('shoppingcart.insert');
-Route::post('/shoppingcart/update', [ShoppingCartController::class, 'update'])
-    ->name('shoppingcart.update');
 Route::get(__('navbar.logout'), function () {
     return redirect()
         ->route('home')
@@ -87,3 +56,31 @@ Route::get(__('navbar.logout'), function () {
             'toast-message' => __('auth.logout-success')
         ]);
 })->name('logout');
+
+/// Orders
+Route::get(__('route.overview') . '/{category?}/{size?}', [OrderController::class, 'overview'])
+    ->name('orders.overview')
+    ->defaults('category', 'bevers')
+    ->defaults('size', 'S');
+
+Route::get(__('route.product') . '/{id}/{size?}', [OrderController::class, 'product'])
+    ->name('orders.product')
+    ->defaults('size', 'S');
+
+// Shopping Cart
+Route::get(__('route.shopping-cart'), [ShoppingCartController::class, 'index'])
+    ->name('shoppingcart.index');
+
+// Checkout
+Route::get(__('route.checkout'), [OrderController::class, 'order'])
+    ->name('orders.order');
+Route::post('/complete-order', [OrderController::class, 'completeOrder'])
+    ->name('orders.complete-order');
+Route::get(__('route.completed-order'), [OrderController::class, 'completedOrder'])
+    ->name('orders.completed');
+
+// Shopping Cart
+Route::post('/shoppingcart/insert/{id}', [ShoppingCartController::class, 'insert'])
+    ->name('shoppingcart.insert');
+Route::post('/shoppingcart/update', [ShoppingCartController::class, 'update'])
+    ->name('shoppingcart.update');
