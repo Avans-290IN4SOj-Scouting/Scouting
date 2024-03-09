f6c67e109b406e64e1e78b446c6bd38c50137730
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-03-09 14:50:25 +0100

    feat(accounts): added browser tests

---
+++ tests/Browser/AccountsControllerTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use App\Models\User;
+
+class AccountsControllerTest extends DuskTestCase
+{
+    public function testView()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('manage-accounts'))
+                ->screenshot('manage-accounts')
+                ->assertUrlIs(env('APP_URL') . __('route.manage_accounts'));
+        });
+    }
+
+    public function testRoleDropdown()
+    {
+        $account = User::first();
+
+        $this->browse(function (Browser $browser) use ($account) {
+            $browser->visit(route('manage-accounts'))
+                ->select('[data-account-email="' . $account->email . '"]', __('roles.admin'))
+                ->assertSee(__('roles.admin'));
+        });
+    }
+
+    public function testModalAppearsAfterRoleChange()
+    {
+        $account = User::first();
+
+        $this->browse(function (Browser $browser) use ($account) {
+            $browser->visit(route('manage-accounts'))
+                ->select('[data-account-email="' . $account->email . '"]', __('roles.admin'))
+                ->click('#saveBtn')
+                ->waitFor('#confirmModal')
+                ->assertVisible('#confirmModal')
+                ->screenshot('manage-accounts-modal');
+        });
+    }
+
+    public function testNoModalDisplayedWithNoChanges()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(route('manage-accounts'))
+                ->click('#saveBtn')
+                ->waitFor('#toast', 10)
+                ->assertVisible('#toast')
+                ->assertSee(__('toast.warning-accounts'))
+                ->screenshot('manage-accounts-warning-toast');
+        });
+    }
+}
+
