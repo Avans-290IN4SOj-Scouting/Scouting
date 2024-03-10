f1d457b56ef9ed616009069793dabf5a809f605b
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-03-10 11:36:10 +0100

    feat(accounts): added darkmode
    View was unreadable without darkmode. Also, other views include darkmode.

--- resources/views/admin/accounts.blade.php
+++ resources/views/admin/accounts.blade.php
@@ -4,25 +4,24 @@
 
 @section('content')
 
-    <body>
     <div class="mx-auto text-center">
-        <h1 class="text-4xl m-8">{{__('accounts.page_title')}}</h1>
+        <h1 class="text-4xl m-8 dark:text-white">{{__('accounts.page_title')}}</h1>
 
-        <div class="w-1/2 mx-auto overflow-hidden border rounded-lg mb-4">
+        <div class="w-1/2 mx-auto overflow-hidden border rounded-lg mb-4 dark:border-gray-700">
             <table id="usersTable" name="usersTable" class="w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
+                <thead class="bg-gray-50 dark:bg-gray-800">
                 <tr>
-                    <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase">
+                    <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase dark:text-gray-400">
                         {{__('accounts.email')}}</th>
-                    <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase">
+                    <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase dark:text-gray-400">
                         {{__('accounts.role')}}</th>
                 </tr>
                 </thead>
                 <tbody class="divide-y divide-gray-200">
 
                 @foreach ($accounts as $account)
-                    <tr class="hover:bg-gray-100">
-                        <td class="px-4 py-2 whitespace-nowrap text-sm font-medium text-gray-800">
+                    <tr class="hover:bg-gray-100 dark:hover:bg-gray-900">
+                        <td class="px-4 py-2 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-white">
                             {{$account['email']}}
                         </td>
                         <td class="px-4 py-2 whitespace-nowrap text-sm font-medium text-gray-800">
@@ -35,10 +34,11 @@
 
                                 <select data-account-email="{{ $account->email }}"
                                         data-old-role="{{ $account->roles->first()->name }}"
-                                        class="block appearance-none w-full border border-gray-300 py-2 px-4 pr-8 rounded leading tight focus:outline-none focus:border-blue-500">
+                                        class="block appearance-none w-full border border-gray-300 py-2 px-4 pr-8 rounded leading tight focus:outline-none focus:border-blue-500 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700">
 
                                     @foreach (trans('roles') as $role => $translatedRole)
-                                        <option value="{{ $role }}" {{ $translatedOldRole === $translatedRole ? "selected" : "" }}>
+                                        <option
+                                            value="{{ $role }}" {{ $translatedOldRole === $translatedRole ? "selected" : "" }}>
                                             {{ __('roles.' . $role) }}
                                         </option>
                                     @endforeach
@@ -60,11 +60,11 @@
 
         <div id="confirmModal" name="confirmModal"
              class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center hidden">
-            <div class="bg-white p-4 rounded shadow-lg">
-                <h3 class="font-bold text-2xl text-gray-800 border-b mb-4 pb-2">{{__('accounts.modal_warning_title')}}
+            <div class="bg-white p-4 rounded shadow-lg dark:bg-gray-800">
+                <h3 class="font-bold text-2xl text-gray-800 border-b mb-4 pb-2 dark:text-white dark:border-gray-600">{{__('accounts.modal_warning_title')}}
                 </h3>
-                <div id="changedAccountsInfo" name="changedAccountsInfo" class="text-gray-800 mb-4"></div>
-                <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t">
+                <div id="changedAccountsInfo" name="changedAccountsInfo" class="text-gray-800 mb-4 dark:text-gray-400"></div>
+                <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-gray-600">
                     <button id="closeModalBtn" name="closeModalBtn"
                             class="mt-4 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">{{__('accounts.close_button')}}</button>
                     <form id="updateRoleForm" action="{{ route('manage-accounts.updateRoles') }}" method="post">
@@ -79,6 +79,5 @@
             </div>
         </div>
     </div>
-</body>
 
 @endsection
