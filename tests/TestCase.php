65fd630e71d9a913daf84e756c761d508e4e94fb
Linde Rus <lsy.rus@student.avans.nl>
2024-03-15 20:36:54 +0100

    build(test-case): setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,17 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->createTestDatabase();
+    }
+
+    private function createTestDatabase(): void
+    {
+        $this->artisan('migrate:fresh', ['--env' => 'testing']);
+        $this->artisan('db:seed', ['--env' => 'testing']);
+    }
 }
