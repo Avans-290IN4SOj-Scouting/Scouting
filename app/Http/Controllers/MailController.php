262f8083c2360d228652fe900ae1e1f576cb92bd
Linde Rus <lsy.rus@student.avans.nl>
2024-03-15 09:51:38 +0100

    refactor(mail-test-data): change name to mail

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -23,122 +23,122 @@ class MailController extends Controller
         return [
             new Mail([
                 "date" => "2021-01-01",
-                "receiver" => "John Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-02",
-                "receiver" => "Jane Doe",
+                "receiver" => "janedoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-03",
-                "receiver" => "John Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-04",
-                "receiver" => "Jane Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-05",
-                "receiver" => "John Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-06",
-                "receiver" => "Jane Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-07",
-                "receiver" => "John Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-08",
-                "receiver" => "Jane Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-07",
-                "receiver" => "John Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-08",
-                "receiver" => "Jane Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-07",
-                "receiver" => "John Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-08",
-                "receiver" => "Jane Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-07",
-                "receiver" => "John Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-08",
-                "receiver" => "Jane Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-07",
-                "receiver" => "John Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-08",
-                "receiver" => "Jane Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-07",
-                "receiver" => "John Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-08",
-                "receiver" => "Jane Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-07",
-                "receiver" => "John Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-08",
-                "receiver" => "Jane Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-07",
-                "receiver" => "John Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-08",
-                "receiver" => "Jane Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-07",
-                "receiver" => "John Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
             new Mail([
                 "date" => "2021-01-08",
-                "receiver" => "Jane Doe",
+                "receiver" => "johndoe@scouting.nl",
                 "subject" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
             ]),
         ];
