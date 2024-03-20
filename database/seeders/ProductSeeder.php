794161d9f60821a7ca9c5109cd756429ad75de58
Roy Vink <roy.vink@gmail.com>
2024-03-20 11:13:27 +0100

    feat(database) Added product description to new migration and seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -13,33 +13,33 @@ class ProductSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        {
+    { {
             $product = Product::create([
                 'name' => 'TestDames',
                 'discount' => 0.00,
                 'image_path' => 'https://placehold.co/200x200',
-                'product_type_id' => 1
+                'product_type_id' => 1,
+                'description' => 'Dit is een testproduct voor dames.',
             ]);
             $product->productSizes()->attach($product->id, ['product_size_id' => 2, 'price' => 23.45]);
             $product->productSizes()->attach($product->id, ['product_size_id' => 3, 'price' => 34.56]);
-        }
-        {
+        } {
             $product = Product::create([
                 'name' => 'TestHeren',
                 'discount' => 0.20,
                 'image_path' => 'https://placehold.co/200x200',
-                'product_type_id' => 2
+                'product_type_id' => 2,
+                'description' => 'Dit is een testproduct voor heren.',
             ]);
             $product->productSizes()->attach($product->id, ['product_size_id' => 1, 'price' => 12.34]);
             $product->productSizes()->attach($product->id, ['product_size_id' => 3, 'price' => 34.56]);
-        }
-        {
+        } {
             $product = Product::create([
                 'name' => 'TestUnisex',
                 'discount' => 0.00,
                 'image_path' => 'https://placehold.co/200x200',
-                'product_type_id' => 3
+                'product_type_id' => 3,
+                'description' => 'Dit is een testproduct voor iedereen.',
             ]);
             $product->productSizes()->attach($product->id, ['product_size_id' => 1, 'price' => 12.34]);
             $product->productSizes()->attach($product->id, ['product_size_id' => 2, 'price' => 23.45]);
