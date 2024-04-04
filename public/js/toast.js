c0cd5225858733e2388582fb80af186b03b22ed6
JrnJ <jeroenjochems@outlook.com>
2024-04-04 13:06:26 +0200

    refactor(toasts): fixed colors

--- public/js/toast.js
+++ public/js/toast.js
@@ -8,27 +8,27 @@ function createToast(text, type) {
     const toastContainer = document.createElement('div');
     toastContainer.classList.add('.norefresh-toast');
 
-    let toastBackgroundColorStyle = '';
+    let toastTypeClass = '';
     switch (type)
     {
         case 'success':
-            toastBackgroundColorStyle = 'max-w-xs bg-teal-100 border border-teal-200 text-sm text-teal-800 rounded-lg dark:bg-teal-800/10 dark:border-teal-900 dark:text-teal-500';
+            toastTypeClass = 'toast-success';
             break;
         case 'error':
-            toastBackgroundColorStyle = 'max-w-xs bg-red-100 border border-red-200 text-sm text-red-800 rounded-lg dark:bg-red-800/10 dark:border-red-900 dark:text-red-500';
+            toastTypeClass =  'toast-error';
             break;
         case 'warning':
-            toastBackgroundColorStyle = 'max-w-xs bg-yellow-100 border border-yellow-200 text-sm text-yellow-800 rounded-lg dark:bg-yellow-800/10 dark:border-yellow-900 dark:text-yellow-500';
+            toastTypeClass =  'toast-warning';
             break;
         default:
-            toastBackgroundColorStyle = 'max-w-xs bg-gray-100 border border-gray-200 text-sm text-gray-800 rounded-lg dark:bg-white/10 dark:border-white/20 dark:text-white';
+            toastTypeClass =  'toast-info';
             break;
     }
 
     const toastHTML =
     `
     <div class="absolute bottom-0 end-0" style="margin: 1rem;">
-        <div class="${toastBackgroundColorStyle}" role="alert">
+        <div class="${toastTypeClass}" role="alert">
             <div class="flex p-4">
                 ${text}
             </div>
