a7b72bfbd83249397585873a5fa6f9fac0b9cdf3
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-04-05 13:03:51 +0200

    feat(accounts): revamp of accountspage (#50)
    * feat(accounts): roles in uppercase
    
    * feat(accounts): added try/catch w/ toast
    
    * feat(accounts): warning when no admin present
    
    Also wrote a test and it works as expected.
    
    testModalAppearsAfterRoleChange broke, could be because of new functionality. Fix is pending
    
    * refactor(manage-acc): dusk test (#43)
    
    * refactor(manage-acc): dusk test
    
    * refactor(test): remove screenshot
    
    * refactor(test): remove comments
    
    * feat(accounts): changed styling table
    
    Also added pagination (not funtional)
    
    * feat(roles): added random subdivision seeder
    
    Controller and view takes this into account
    
    * fix(accounts): active roles displayed
    
    * refactor(acc-promo): make accessible and responsive (#45)
    
    * feat(acc-promo): responsive progress
    
    * Revert "Merge branch 'feat/acc-promo-SOJ-227' into acc-promo/accessible-responsive"
    
    This reverts commit 2634a0fb7674c8a6a2897852cf0e22aa3feed65b, reversing
    changes made to ceff56f8cde6ea0472d145241c3e78dfe1b413dc.
    
    * refactor(button): accessible + z-index toast
    
    * refactor(style): to classes
    
    * feat(pagination + sorting): add (#46)
    
    * feat(pagination)
    
    * feat(sortable): email
    
    * refactor(paginate): amount of accounts
    
    * refactor(pagination): fix sorting
    
    * fix(roles): role update fix (#48)
    
    * feat(seeder): added user with multiple roles
    
    also made admin@admin.com valid
    
    * temp(roles): temp commit
    
    * fix(roles): roles can be updated again
    
    * fix(roles): admin check fixed
    
    * fix(roles): feedback applied
    
    where -> wherenot
    removed console.log
    
    * fix(roles): semantic names for modal
    
    * refactor(roles): logged in admin not in list
    
    * refactor(roles): user check in controller
    
    * fix(roles): merge conflict controller
    
    * fix(dropdown): fix dropdown cutoff
    
    * fix(test): fixed feature test
    
    * fix(test): fixed roleDropdown test
    
    * fix(test): fixed testModalAppearsAfterRoleChange
    
    * refactor(test): removal test
    
    Test was redundant with introduction new feature
    
    * fix(accounts): role update and removal admin check
    
    Admin check is removed due to the fact that the admin role of the current logged in admin cannot be changed. Also, users with no changed roles are not displayed in modal
    
    * refactor(accounts): applied review feedback
    
    ---------
    
    Co-authored-by: Linde Rus <130562052+linderus@users.noreply.github.com>

--- lang/nl/toast.php
+++ lang/nl/toast.php
@@ -3,6 +3,9 @@
 return [
     "warning-accounts" => "Er zijn geen rollen aangepast!",
     "success-role-update" => "Rollen succesvol opgeslagen!",
+    "error-account-loading" => "Accounts kunnen niet opgehaald worden!",
+    "error-account-saving" => "Rollen kunnen niet geüpdate worden!",
+    "warning-no-admins" => "Er moet minstens één admin aangewezen zijn!",
     "order-no-products" => "U heeft geen producten in uw winkelwagen!",
 ];
 
--- resources/views/admin/accounts.blade.php
+++ resources/views/admin/accounts.blade.php
@@ -5,83 +5,88 @@
 @endphp
 
 @section('content')
+    <h1 class="text-4xl m-8 dark:text-white">{{__('accounts.page_title')}}</h1>
 
-    <div class="mx-auto text-center">
-        <h1 class="text-4xl m-8 dark:text-white">{{__('accounts.page_title')}}</h1>
-
-        <div class="w-1/2 mx-auto overflow-hidden border rounded-lg mb-4 dark:border-gray-700">
-            <table id="usersTable" name="usersTable" class="w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50 dark:bg-gray-800">
+    <div class="flex flex-col">
+        <div class="p-1.5 min-w-full inline-block align-middle">
+            <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                <thead>
                 <tr>
-                    <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase dark:text-gray-400">
-                        {{__('accounts.email')}}</th>
-                    <th scope="col" class="px-6 py-3 text-xs font-medium text-gray-500 uppercase dark:text-gray-400">
-                        {{__('accounts.role')}}</th>
+                    <th scope="col"
+                        class="w-4/5 px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                        @sortablelink('email', __('accounts.email'))
+                    </th>
+                    <th scope="col"
+                        class="w-1/5 px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                        {{ __('accounts.role') }}
+                    </th>
                 </tr>
                 </thead>
-                <tbody class="divide-y divide-gray-200">
-
-                @foreach ($accounts as $account)
-                    <tr class="hover:bg-gray-100 dark:hover:bg-gray-900">
-                        <td class="px-4 py-2 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-white">
-                            {{$account['email']}}
+                <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+                @foreach($accounts as $account)
+                    <tr>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+                            {{ $account['email'] }}
                         </td>
-                        <td class="px-4 py-2 whitespace-nowrap text-sm font-medium text-gray-800">
-                            <div class="relative inline-block text-left">
-
-                                @php
-                                    $oldRole = $account->roles->first() ? $account->roles->first()->name : null;
-                                    $translatedOldRole = $oldRole ? __('roles.' . $oldRole) : null;
-                                @endphp
-
-                                <label hidden>{{ __('accounts.role')  }}</label>
-                                <select data-account-email="{{ $account->email }}"
-                                        data-old-role="{{ $account->roles->first()->name }}"
-                                        class="block appearance-none w-full border border-gray-300 py-2 px-4 pr-8 rounded leading tight focus:outline-none focus:border-blue-500 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700">
-
-                                    @foreach (trans('roles') as $role => $translatedRole)
-                                        <option
-                                                value="{{ $role }}" {{ $translatedOldRole === $translatedRole ? "selected" : "" }}>
-                                            {{ __('roles.' . $role) }}
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+                            <div id="selectRole-div" class="relative"
+                                 data-account-email="{{ $account->email }}"
+                                 data-old-roles="{{ json_encode($account->roles->pluck('name')) }}"
+                                 style="width: 250px;">
+                                <label for="selectRole" hidden>{{ __('accounts.role')  }}</label>
+                                <select id="selectRole" multiple
+                                        data-hs-select='{
+                                                  "placeholder": "{{ __('accounts.multiple_select_placeholder') }}",
+                                                  "toggleTag": "<button type=\"button\"></button>",
+                                                  "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 px-4 pe-9 flex text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:border-blue-500 focus:ring-blue-500 before:absolute before:inset-0 before:z-[1] dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600",
+                                                  "dropdownClasses": "mt-2 z-50 w-full max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto dark:bg-slate-900 dark:border-gray-700",
+                                                  "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:bg-slate-900 dark:hover:bg-slate-800 dark:text-gray-200 dark:focus:bg-slate-800",
+                                                  "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"flex-shrink-0 size-3.5 text-blue-600 dark:text-blue-500\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>"
+                                                }' class="hidden">
+                                    @foreach($roles as $role)
+                                        <option value="{{ $role->name }}"
+                                                data-translated-name="{{ __('roles.' . $role->name) }}" {{ in_array($role->name, $account->roles->pluck('name')->toArray()) ? 'selected' : '' }}>
+                                            {{ __('roles.' . $role->name) }}
                                         </option>
                                     @endforeach
-
                                 </select>
                             </div>
                         </td>
                     </tr>
                 @endforeach
-
                 </tbody>
             </table>
         </div>
+    </div>
 
-        <div class="fixed bottom-0 right-0 m-4">
-            <button id="saveBtn" name="saveBtn"
-                    class="saveBtn bg-blue-500 text-white px-8 py-4 rounded hover:bg-blue-600">{{__('accounts.save_button')}}</button>
-        </div>
+    {{ $accounts->appends(\Request::except('page'))->links('components.pagination') }}
 
-        <div id="confirmModal" name="confirmModal"
-             class="confirmModal fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center hidden">
-            <div class="bg-white p-4 rounded shadow-lg dark:bg-gray-800">
-                <h3 class="font-bold text-2xl text-gray-800 border-b mb-4 pb-2 dark:text-white dark:border-gray-600">{{__('accounts.modal_warning_title')}}
-                </h3>
-                <div id="changedAccountsInfo" name="changedAccountsInfo"
-                     class="text-gray-800 mb-4 dark:text-gray-400"></div>
-                <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-gray-600">
-                    <button id="closeModalBtn" name="closeModalBtn"
-                            class="mt-4 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">{{__('accounts.close_button')}}</button>
-                    <form id="updateRoleForm" action="{{ route('manage-accounts.updateRoles') }}" method="post">
-                        @csrf
-                        <input type="hidden" id="userRoles" name="userRoles" value="">
-                        <button type="button" id="confirmModalBtn" name="confirmModalBtn"
-                                class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                            {{__('accounts.confirm_button')}}
-                        </button>
-                    </form>
-                </div>
+    <div class="fixed bottom-0 right-0 m-4">
+        <button id="saveBtn" name="saveBtn" type="button"
+                class="saveBtn p-4 sm:p-5 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+            {{__('accounts.save_button')}}
+        </button>
+    </div>
+
+    <div id="confirmModal" name="confirmModal"
+         class="confirmModal fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center hidden">
+        <div class="bg-white p-4 rounded shadow-lg dark:bg-gray-800">
+            <h3 class="font-bold text-2xl text-gray-800 border-b mb-4 pb-2 dark:text-white dark:border-gray-600">{{__('accounts.modal_warning_title')}}
+            </h3>
+            <div id="changedAccountsInfo" name="changedAccountsInfo"
+                 class="text-gray-800 mb-4 dark:text-gray-400"></div>
+            <div class="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-gray-600">
+                <button id="closeModalBtn" name="closeModalBtn"
+                        class="mt-4 bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600">{{__('accounts.close_button')}}</button>
+                <form id="updateRoleForm" action="{{ route('manage-accounts.updateRoles') }}" method="post">
+                    @csrf
+                    <input type="hidden" id="userRoles" name="userRoles" value="">
+                    <button type="button" id="confirmModalBtn" name="confirmModalBtn"
+                            class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                        {{__('accounts.confirm_button')}}
+                    </button>
+                </form>
             </div>
         </div>
     </div>
-
 @endsection
