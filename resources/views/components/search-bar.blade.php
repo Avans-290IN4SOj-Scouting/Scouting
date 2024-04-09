52b49cd7e2e5a0678765ea385054285d22b6ec9b
Linde Rus <lsy.rus@student.avans.nl>
2024-04-09 23:49:35 +0200

    feat(search-bar): create component

---
+++ resources/views/components/search-bar.blade.php
@@ -0,0 +1,15 @@
+<div class="relative" style="width: fit-content; border: 1px solid gainsboro; border-radius: 0.5rem;">
+    <div class="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-3.5">
+        <svg class="flex-shrink-0 size-4 text-gray-400 dark:text-white/60" xmlns="http://www.w3.org/2000/svg"
+             width="24"
+             height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+             stroke-linecap="round"
+             stroke-linejoin="round">
+            <circle cx="11" cy="11" r="8"></circle>
+            <path d="m21 21-4.3-4.3"></path>
+        </svg>
+    </div>
+    <input
+        class="py-3 ps-10 pe-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+        type="text" placeholder="{{ $placeholder }}" value="" data-hs-combo-box-input="">
+</div>
