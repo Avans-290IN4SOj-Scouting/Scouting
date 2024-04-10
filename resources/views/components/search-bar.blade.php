3c0b7b3a98de80e5eb05d49bed4e2ff49671b67d
Linde Rus <lsy.rus@student.avans.nl>
2024-04-10 19:05:39 +0200

    refactor(search-bar): add label

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -12,6 +12,7 @@
                 <path d="m21 21-4.3-4.3"></path>
             </svg>
         </div>
+        <label for="search" class="sr-only">{{ $placeholder }}</label>
         <input
             class="py-3 ps-10 pe-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
             id="search" name="q" type="text" placeholder="{{ $placeholder }}" value="{{ $search }}">
