f57f8046041fa6d4f1d1eb6cb6575bf69601a838
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-04-11 21:31:02 +0200

    feat(orderlines): addition image_path column (#58)
    * feat(orderlines): addition image_path column
    
    * Revert "feat(orderlines): addition image_path column"
    
    This reverts commit 52e34626d0ddaa6afeef416a1a00d991ee26ed04.
    
    * refactor(orderlines): added separate migration
    
    product_image_path is now added in a separate migration.
    
    * feat(orderlines): added seed for image_path

---
+++ database/migrations/2024_04_11_212344_add_product_image_path_column.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('order_lines', function (Blueprint $table) {
+            $table->string('product_image_path')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('order_lines', function (Blueprint $table) {
+            $table->dropColumn('product_image_path');
+        });
+    }
+};
