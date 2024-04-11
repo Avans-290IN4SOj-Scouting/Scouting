5f99d6d4c32053ef96f9467351cf3d24812dfd5c
Linde Rus <lsy.rus@student.avans.nl>
2024-04-11 20:32:16 +0200

    refactor(filters): remember previous filter

--- resources/views/admin/accounts.blade.php
+++ resources/views/admin/accounts.blade.php
@@ -28,14 +28,46 @@
                         {{__('manage-accounts/accounts.remove_filters_button')}}
                     </a>
                 </div>
+
+                <!-- Add these hidden fields to remember the sort direction and column -->
+                <input type="hidden" name="sort" value="{{ Request::get('sort') }}">
+                <input type="hidden" name="direction" value="{{ Request::get('direction') }}">
             </form>
 
             <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                 <thead>
                 <tr>
                     <th scope="col"
-                        class="w-4/5 px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                        class="w-4/5 px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase flex items-left gap-0.5">
                         @sortablelink('email', __('manage-accounts/accounts.email'))
+                        @if (Request::get('sort') == 'email')
+                            @if (Request::get('direction') == 'desc')
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                      <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m19.5 8.25-7.5 7.5-7.5-7.5"/>
+                                    </svg>
+                                </span>
+                            @else
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                      <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m4.5 15.75 7.5-7.5 7.5 7.5"/>
+                                    </svg>
+                                </span>
+                            @endif
+                        @else
+                            <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 stroke-width="1.5"
+                                 stroke="currentColor" class="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"/>
+                            </svg>
+                        </span>
+                        @endif
                     </th>
                     <th scope="col"
                         class="w-1/5 px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
