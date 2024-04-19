523e4f4da47f8526f02611f3a953fea40c15684d
Anton Jagers <36148924+adjagers@users.noreply.github.com>
2024-04-19 17:11:20 +0200

    multi-select change for dusk test

--- resources/views/partials/_select.blade.php
+++ resources/views/partials/_select.blade.php
@@ -1,9 +1,8 @@
-<label class="block text-gray-700 font-semibold">{{ $label }}</label>
+<label class="select block text-gray-700 font-semibold">{{ $label }}</label>
 <div class="relative">
-    <select multiple name="{{ $name }}[]"
+    <select dusk="multiple-select-{{__($name)}}" class= "{{$class}}" multiple name="{{ $name }}[]"
             data-hs-select='{
         "placeholder": "{{ $placeholder }}",
-        "toggleTag": "<button type=\"button\"></button>",
         "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 px-4 pe-9 flex text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:border-blue-500 focus:ring-blue-500 before:absolute before:inset-0 before:z-[1] dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600",
         "dropdownClasses": "mt-2 z-50 w-full max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto dark:bg-slate-900 dark:border-gray-700",
         "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:bg-slate-900 dark:hover:bg-slate-800 dark:text-gray-200 dark:focus:bg-slate-800",
@@ -11,9 +10,11 @@
     }'>
         <option value="">Kies</option>
         @foreach ($options as $option)
-            <option {{ isset($chosenGroups) && in_array($option, $chosenGroups->pluck('name')->toArray()) ? 'selected' : '' }}>{{ $option }}</option>
+            <option value={{$option}} {{ isset($chosenGroups) && in_array($option, $chosenGroups->pluck('name')->toArray()) ? 'selected' : '' }}>{{ $option }}</option>
         @endforeach
     </select>
+
+
     <div class="absolute top-1/2 end-3 -translate-y-1/2">
         <svg class="flex-shrink-0 size-3.5 text-gray-500 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg"
              width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
@@ -23,4 +24,3 @@
         </svg>
     </div>
 </div>
-
