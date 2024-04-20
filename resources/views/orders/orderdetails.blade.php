e60c3fcbdb6dffe17b76352b98321188464c3ec0
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-04-20 19:44:42 +0200

    feat(orders): improved layout
    Layout is in accordance with wireframe design.

--- resources/views/orders/orderdetails.blade.php
+++ resources/views/orders/orderdetails.blade.php
@@ -8,15 +8,37 @@
 
     <!-- TODO: replace hardcoded string w/ attributes and localization -->
 
+    <div class="text-sm">
+        <a href="#" class="flex flex-row items-center hover:text-blue-600 dark:text-white dark:fill-white">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M15 18L9 12L15 6" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <div>Terug naar bestellingen</div>
+        </a>
+    </div>
+
     <div class="m-4">
-        <div class="flex flex-row justify-between w-full p-6 rounded-xl border dark:text-white dark:border-gray-700">
-            <div>Bestelling: 123234</div>
-            <div>Besteldatum: 0001-01-01</div>
+        <div class="flex flex-row justify-between items-center w-full dark:text-white">
+            <div>
+                <div class="text-4xl">Bestelling 123234</div>
+            </div>
+            <div class="flex flex-row justify-center gap-4 items-center">
+                <div>Besteldatum</div>
+                <div class="p-2 border rounded dark:border-gray-700">01-01-0001</div>
+            </div>
+            <div class="flex flex-row justify-center gap-4 items-center">
+                <div>Totaalbedrag</div>
+                <div class="p-2 border rounded dark:border-gray-700">€99,99</div>
+            </div>
             <div>Geannuleerd</div>
         </div>
     </div>
 
-    @for($i = 0; $i < 4; $i++)
+    <div class="m-4">
+        <button class="bg-red-600 text-white rounded p-2 hover:bg-red-900">Annuleer bestelling</button>
+    </div>
+
+    @for($i = 0; $i < 3; $i++)
 
         <div class="m-4">
             <div class="flex flex-row justify-between items-center w-full p-6 rounded-xl border dark:text-white dark:border-gray-700">
@@ -24,7 +46,7 @@
                     <img src="https://placehold.co/200" alt="product image">
                 </div>
                 <div>
-                    <a href="#" action="" class="font-bold">
+                    <a href="#" class="font-bold">
                         Product titel
                     </a>
                 </div>
@@ -35,11 +57,5 @@
         </div>
 
     @endfor
-
-    <div class="m-4">
-        <div class="flex flex-row justify-center w-full p-6 rounded-xl border dark:text-white dark:border-gray-700">
-            <div>Totaal: €99,99</div>
-        </div>
-    </div>
 
 @endsection
