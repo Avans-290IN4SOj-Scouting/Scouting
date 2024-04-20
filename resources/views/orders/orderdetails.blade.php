cb186ec1f80ecf7d13247b1c32d33f0f767705df
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-04-20 17:17:56 +0200

    feat(orders): addition darkmode

--- resources/views/orders/orderdetails.blade.php
+++ resources/views/orders/orderdetails.blade.php
@@ -9,8 +9,8 @@
     <!-- TODO: replace hardcoded string w/ attributes and localization -->
 
     <div class="m-4">
-        <div class="flex flex-row justify-between w-full bg-white p-6 rounded-xl">
-            <div>Bestelling 1234</div>
+        <div class="flex flex-row justify-between w-full p-6 rounded-xl border dark:text-white dark:border-gray-700">
+            <div>Bestelling: 123234</div>
             <div>Besteldatum: 0001-01-01</div>
             <div>Geannuleerd</div>
         </div>
@@ -19,11 +19,15 @@
     @for($i = 0; $i < 4; $i++)
 
         <div class="m-4">
-            <div class="flex flex-row justify-between items-center w-full bg-white p-6 rounded-xl">
+            <div class="flex flex-row justify-between items-center w-full p-6 rounded-xl border dark:text-white dark:border-gray-700">
                 <div>
                     <img src="https://placehold.co/200" alt="product image">
                 </div>
-                <div>Product titel</div>
+                <div>
+                    <a href="#" action="" class="font-bold">
+                        Product titel
+                    </a>
+                </div>
                 <div>M</div>
                 <div>€10,00</div>
                 <div>2 stuks</div>
@@ -33,7 +37,7 @@
     @endfor
 
     <div class="m-4">
-        <div class="flex flex-row justify-center w-full bg-white p-6 rounded-xl">
+        <div class="flex flex-row justify-center w-full p-6 rounded-xl border dark:text-white dark:border-gray-700">
             <div>Totaal: €99,99</div>
         </div>
     </div>
