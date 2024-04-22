cc61bbbedc4f9083a416e9c9d41de58711507d64
lilHerbie <ht.alting@student.avans.nl>
2024-04-22 10:27:27 +0200

    enum fix

--- app/Enum/OrderStatus.php
+++ app/Enum/OrderStatus.php
@@ -14,36 +14,36 @@ enum OrderStatus: string
     public static function localised(): array
     {
         return [
-            self::Cancelled->value => __('delivery_status.cancelled'),
-            self::PaymentRefunded->value => __('delivery_status.payment_refunded'),
-            self::AwaitingPayment->value => __('delivery_status.awaiting_payment'),
-            self::Processing->value => __('delivery_status.processing'),
-            self::Delivered->value => __('delivery_status.delivered'),
-            self::Finalized->value => __('delivery_status.finalized'),
+            self::Cancelled->value => __('order_status.cancelled'),
+            self::PaymentRefunded->value => __('order_status.payment_refunded'),
+            self::AwaitingPayment->value => __('order_status.awaiting_payment'),
+            self::Processing->value => __('order_status.processing'),
+            self::Delivered->value => __('order_status.delivered'),
+            self::Finalized->value => __('order_status.finalized'),
         ];
     }
 
     public static function delocalised(string $value): self
     {
         return match ($value) {
-            __('delivery_status.cancelled') => self::Cancelled,
-            __('delivery_status.payment_refunded') => self::PaymentRefunded,
-            __('delivery_status.awaiting_payment') => self::AwaitingPayment,
-            __('delivery_status.processing') => self::Processing,
-            __('delivery_status.delivered') => self::Delivered,
-            __('delivery_status.finalized') => self::Finalized,
+            __('order_status.cancelled') => self::Cancelled,
+            __('order_status.payment_refunded') => self::PaymentRefunded,
+            __('order_status.awaiting_payment') => self::AwaitingPayment,
+            __('order_status.processing') => self::Processing,
+            __('order_status.delivered') => self::Delivered,
+            __('order_status.finalized') => self::Finalized,
         };
     }
 
     public static function localisedValue(string $value): string
     {
         return match ($value) {
-            self::Cancelled->value => __('delivery_status.cancelled'),
-            self::PaymentRefunded->value => __('delivery_status.payment_refunded'),
-            self::AwaitingPayment->value => __('delivery_status.awaiting_payment'),
-            self::Processing->value => __('delivery_status.processing'),
-            self::Delivered->value => __('delivery_status.delivered'),
-            self::Finalized->value => __('delivery_status.finalized'),
+            self::Cancelled->value => __('order_status.cancelled'),
+            self::PaymentRefunded->value => __('order_status.payment_refunded'),
+            self::AwaitingPayment->value => __('order_status.awaiting_payment'),
+            self::Processing->value => __('order_status.processing'),
+            self::Delivered->value => __('order_status.delivered'),
+            self::Finalized->value => __('order_status.finalized'),
         };
     }
 }
