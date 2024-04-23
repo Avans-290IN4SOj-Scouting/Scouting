2147ab64ff0ae0ad1904cfb3e0447b373a6bea72
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-04-23 19:21:04 +0200

    refactor(orders): revamp responsiveness

--- resources/views/orders/orderdetails.blade.php
+++ resources/views/orders/orderdetails.blade.php
@@ -17,34 +17,38 @@
         </a>
     </div>
 
-    <div class="shadow p-4 m-4 rounded-xl">
+    <div class="shadow p-4 rounded-xl">
         <div class="m-4">
-            <div class="flex flex-col gap-4 lg:flex-row justify-between items-center w-full dark:text-white">
+            <div class="flex flex-col gap-4 justify-between md:flex-row w-full dark:text-white">
                 <div class="mb-2 sm:mb-0">
                     <div class="text-4xl">{{ __('orders/orders.order') }} 123234</div>
+                    <div class="mt-4 flex flex-row lg:justify-normal items-center">
+                        <button
+                            class="bg-red-600 text-white rounded p-2 hover:bg-red-900">{{ __('orders/orders.cancel-order') }}</button>
+                    </div>
                 </div>
-                <div class="flex flex-col sm:flex-row justify-center gap-4 items-center">
-                    <div>{{ __('orders/orders.order-date') }}</div>
-                    <div class="p-2 rounded shadow dark:border dark:border-gray-700">01-04-2024</div>
+                <div class="flex md:flex-row justify-between gap-10 lg:w-3/5">
+                    <div class="flex flex-col md:flex-row justify-center gap-2 items-baseline">
+                        <div>{{ __('orders/orders.order-date') }}</div>
+                        <div class="p-2 rounded shadow h-11 dark:border dark:border-gray-700">01-04-2024</div>
+                    </div>
+                    <div class="flex flex-col md:flex-row justify-center gap-2 items-baseline">
+                        <div>{{ __('orders/orders.total-price') }}</div>
+                        <div class="p-2 rounded shadow h-11 dark:border dark:border-gray-700">€60,00</div>
+                    </div>
+                    <div class="md:mt-2.5">Geannuleerd</div>
                 </div>
-                <div class="flex flex-col sm:flex-row justify-center gap-4 items-center">
-                    <div>{{ __('orders/orders.total-price') }}</div>
-                    <div class="p-2 rounded shadow dark:border dark:border-gray-700">€60,00</div>
-                </div>
-                <div>Geannuleerd</div>
             </div>
         </div>
 
-        <div class="m-4 flex flex-row justify-center lg:justify-normal items-center">
-            <button
-                class="bg-red-600 text-white rounded p-2 hover:bg-red-900">{{ __('orders/orders.cancel-order') }}</button>
-        </div>
 
         @for($i = 0; $i < 3; $i++)
 
             <div class="m-4">
-                <div class="flex flex-col gap-1 sm:gap-0 pb-1 sm:pb-0 sm:flex-row justify-between items-center sm:pr-6 rounded-xl border dark:text-white dark:border-gray-700">
-                    <img src="https://placehold.co/200" alt="product image" class="rounded-t-xl sm:rounded-tr-none sm:rounded-l-xl w-full sm:w-fit h-auto">
+                <div
+                    class="flex flex-col gap-1 sm:gap-0 pb-1 sm:pb-0 sm:flex-row justify-between items-center sm:pr-6 rounded-xl border dark:text-white dark:border-gray-700">
+                    <img src="https://placehold.co/200" alt="product image"
+                         class="rounded-t-xl sm:rounded-tr-none sm:rounded-l-xl w-full sm:w-fit h-auto">
                     <div class="">
                         <a href="#" class="font-bold">
                             Product titel
