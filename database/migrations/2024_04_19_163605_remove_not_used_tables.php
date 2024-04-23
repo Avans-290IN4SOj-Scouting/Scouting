daed07dac533a3f776c4019868ab4b8be2e633c7
Linde Rus <lsy.rus@student.avans.nl>
2024-04-23 19:14:24 +0200

    refactor(migration): remove unused tables

--- database/migrations/2024_04_19_163605_remove_not_used_tables.php
+++ database/migrations/2024_04_19_163605_remove_not_used_tables.php
@@ -12,6 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::dropIfExists('order_statuses');
+        Schema::dropIfExists('delivery_statuses');
+        Schema::dropIfExists('delivery_states');
     }
 
     /**
