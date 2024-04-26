3e1cc582156a8f7f7b115c6fd6bf0d10d51f6e4a
JrnJ <jeroenjochems@outlook.com>
2024-04-26 16:46:36 +0200

    refactor(db): db table fix

--- database/migrations/2024_04_17_185008_add_userid_to_order.php
+++ database/migrations/2024_04_17_185008_add_userid_to_order.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            // $table->string('user_id');
+            $table->foreignId('user_id')->constrained();
         });
     }
 
@@ -21,7 +21,7 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            // $table->dropColumn('user_id');
+            $table->dropColumn('user_id');
         });
     }
 };
