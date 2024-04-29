9a1d7dc2578244199b9befe1ba4a18b672644efa
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-04-29 18:31:48 +0200

    feat(orderdetails): actual orderdetails displayed

--- app/Http/Controllers/OrderDetailsController.php
+++ app/Http/Controllers/OrderDetailsController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Enum\DeliveryStatus;
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\OrderLine;
 use Illuminate\Http\Request;
 
 class OrderDetailsController extends Controller
 {
-    public function orderDetails() {
-        return view('orders.orderdetails');
+    public function orderDetails()
+    {
+        // hardcoded value to test
+        $orderId = 1;
+
+        $order = Order::where('id', $orderId)->first();
+        $order->order_date = new \DateTime($order->order_date);
+        $order->status = DeliveryStatus::localisedValue($order->status);
+
+        $orderLines = OrderLine::with('product')->where('order_id', $orderId)->get();
+
+        $totalPrice = 0;
+
+        foreach ($orderLines as $orderLine) {
+            $totalPrice += $orderLine->product_price * $orderLine->amount;
+        }
+
+        return view('orders.orderdetails', ['order' => $order, 'orderLines' => $orderLines, 'totalPrice' => $totalPrice]);
     }
 }
--- resources/views/orders/orderdetails.blade.php
+++ resources/views/orders/orderdetails.blade.php
@@ -21,7 +21,7 @@
         <div class="m-4">
             <div class="flex flex-col gap-4 justify-between md:flex-row w-full dark:text-white">
                 <div class="flex flex-col justify-normal mb-2 sm:mb-0">
-                    <div class="text-4xl">{{ __('orders/orders.order') }} 123234</div>
+                    <div class="text-4xl">{{ __('orders/orders.order') }} {{ $order->id }}</div>
                     <div class="mt-4">
                         <button
                             class="bg-red-600 text-white rounded p-2 hover:bg-red-900">{{ __('orders/orders.cancel-order') }}</button>
@@ -30,16 +30,15 @@
                 <div class="flex flex-col sm:flex-row justify-between gap-10 lg:w-3/5">
                     <div class="flex flex-row gap-2 items-baseline">
                         <div>{{ __('orders/orders.order-date') }}</div>
-                        <div class="p-2 rounded shadow h-11 dark:border dark:border-gray-700">01-04-2024</div>
+                        <div class="p-2 rounded shadow h-11 dark:border dark:border-gray-700">{{ App\Helpers\DateFormatter::format($order->order_date) }}</div>
                     </div>
                     <div class="flex flex-row gap-2 items-baseline">
                         <div>{{ __('orders/orders.total-price') }}</div>
-                        <div class="p-2 rounded shadow h-11 dark:border dark:border-gray-700">€60,00</div>
+                        <div class="p-2 rounded shadow h-11 dark:border dark:border-gray-700">{{ $totalPrice }}</div>
                     </div>
-                    <!-- Added to test colors, currently hardcoded -->
                     <div class="sm:mt-2.5">
-                        <p class="{{ \App\Enum\DeliveryStatus::delocalised('Geannuleerd') }}">
-                            Geannuleerd
+                        <p class="{{ \App\Enum\DeliveryStatus::delocalised($order->status) }}">
+                            {{ $order->status }}
                         </p>
                     </div>
                 </div>
@@ -47,25 +46,25 @@
         </div>
 
 
-        @for($i = 0; $i < 3; $i++)
+        @foreach($orderLines as $orderLine)
 
             <div class="m-4">
                 <div
                     class="flex flex-col gap-1 sm:gap-0 pb-1 sm:pb-0 sm:flex-row justify-between items-center sm:pr-6 rounded-xl border dark:text-white dark:border-gray-700">
-                    <img src="https://placehold.co/200" alt="product image"
-                         class="rounded-t-xl sm:rounded-tr-none sm:rounded-l-xl w-full sm:w-fit h-auto">
+                    <img src="{{ $orderLine->product_image_path }}" alt="Image of {{ $orderLine->product->name }}"
+                         class="rounded-t-xl sm:rounded-tr-none sm:rounded-l-xl w-full sm:w-48 h-auto">
                     <div class="">
                         <a href="#" class="font-bold">
-                            Product titel
+                            {{ $orderLine->product->name }}
                         </a>
                     </div>
-                    <div>M</div>
-                    <div>€10,00</div>
-                    <div>2 {{ __('orders/orders.amount-items') }}</div>
+                    <div>{{ $orderLine->product_size }}</div>
+                    <div>{{ $orderLine->product_price }}</div>
+                    <div>{{ $orderLine->amount }} {{ __('orders/orders.amount-items') }}</div>
                 </div>
             </div>
 
-        @endfor
+        @endforeach
     </div>
 
 @endsection
