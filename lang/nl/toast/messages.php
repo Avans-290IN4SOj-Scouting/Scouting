94b88d089a380ef18ad90cd5e934592802b7c24d
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-05-02 19:57:53 +0200

    refactor(orderdetails): made desc more general

--- lang/nl/toast/messages.php
+++ lang/nl/toast/messages.php
@@ -8,7 +8,7 @@ return [
     "warning-no-admins" => "Er moet minstens één admin aangewezen zijn!",
     "order-no-products" => "U heeft geen producten in uw winkelwagen!",
     "error-order-not-found" => "De bestelling kan niet gevonden worden!",
-    "error-order-not-cancelled" => "Er is al betaald voor de bestelling!",
+    "error-order-not-cancelled" => "Order kan niet meer geannuleerd worden!",
     "success-order-cancelled" => "Bestelling succesvol geannuleerd!",
     "error-general" => "Er is iets mis gegaan!",
 ];
