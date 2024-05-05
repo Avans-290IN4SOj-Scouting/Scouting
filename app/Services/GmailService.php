4c89d39369f53826543bda6ac98f8af1dc85b496
JrnJ <jeroenjochems@outlook.com>
2024-05-05 13:21:40 +0200

    test6

--- app/Services/GmailService.php
+++ app/Services/GmailService.php
@@ -30,7 +30,8 @@ class GmailService
         //     $this->client->setRedirectUri(route('gmail.auth-callback'));
 
         //     // Credentials
-        //     $this->client->setAuthConfig(storage_path('app/sensitive/gmail/scoutingazg-gmail-api-oauth-credentials.json'));
+            $this->client->setAuthConfig(storage_path('app/sensitive/gmail/scoutingazg-gmail-api-oauth-credentials.json'));
+            // $this->client->setAuthConfig(Storage::disk('local')->get('sensitive/gmail/scoutingazg-gmail-api-oauth-credentials.json'));
         //     $this->client->setSubject($this->email);
 
         //     // Offline access means the gmail api will only have to authenticate once / refresh token
