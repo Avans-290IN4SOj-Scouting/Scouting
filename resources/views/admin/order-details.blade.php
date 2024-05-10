da598f46aa9da2c271e47d4cbffda84c87f8283d
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-05-10 19:42:13 +0200

    refactor(status): removal redundant id

--- resources/views/admin/order-details.blade.php
+++ resources/views/admin/order-details.blade.php
@@ -64,7 +64,7 @@
                                    class="block text-sm font-medium mb-2 dark:text-white">{{ __('manage-orders/orders.status') }}</label>
                             <select id="status" data-hs-select='{
                                 "placeholder": "Select option...",
-                                "toggleTag": "<button id=\"test\" type=\"button\"></button>",
+                                "toggleTag": "<button type=\"button\"></button>",
                                 "toggleClasses": "hs-select-disabled:pointer-events-none relative py-3 px-4 pe-9 flex text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:border-blue-500 focus:ring-blue-500 before:absolute before:inset-0 before:z-[1] dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400",
                                 "dropdownClasses": "mt-2 z-50 w-full max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 dark:bg-slate-900 dark:border-gray-700",
                                 "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:hover:bg-slate-800 dark:text-neutral-200 dark:focus:bg-slate-800",
