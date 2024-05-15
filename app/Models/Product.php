ce4b9a9db2e4f0b655d8fab49560e6fcf4e708f6
Anton Jagers <36148924+adjagers@users.noreply.github.com>
2024-05-15 17:35:35 +0200

    refactor(model) no usage code

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -48,18 +48,4 @@ class Product extends Model
     {
         return $this->belongsToMany(Group::class, 'product_group');
     }
-
-    public function setPriceForSize(mixed $priceForSize)
-    {
-        $this->priceForSize = $priceForSize;
-    }
-
-    public function setGroups($groups)
-    {
-        if (is_array($groups)) {
-            $this->groups = $groups;
-        } else {
-            $this->groups = [$groups];
-        }
-    }
 }
