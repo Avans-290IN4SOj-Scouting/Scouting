f0a4016fb59aef525843957f496fbf3140a18fe4
VindSent <vincentvanhintum@gmail.com>
2024-05-16 22:33:10 +0200

    revert(admin): changed seeders

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,30 +4,78 @@ namespace Database\Seeders;
 
 use App\Models\Product;
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
 class ProductSeeder extends Seeder
 {
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $productNames = ['Wandelschoenen', 'Vest', 'Shirt', 'Broek', 'Hoed', 'Sjaal', 'Riem'];
-
-        foreach ($productNames as $productName) {
+        {
             $product = Product::create([
-                'name' => $productName,
+                'name' => 'TestAll',
                 'image_path' => '/images/products/placeholder.png',
             ]);
+            $product->productSizes()->attach($product->id, ['product_size_id' => 1, 'price' => 23.45]);
+            $product->productSizes()->attach($product->id, ['product_size_id' => 2, 'price' => 34.56]);
+            $product->productSizes()->attach($product->id, ['product_size_id' => 3, 'price' => 45.67]);
+            $product->productSizes()->attach($product->id, ['product_size_id' => 4, 'price' => 56.78]);
+            $product->productSizes()->attach($product->id, ['product_size_id' => 5, 'price' => 67.89]);
 
-            for ($i = 1; $i <= 5; $i++) {
-                $product->productSizes()->attach($product->id, ['product_size_id' => $i, 'price' => 23.45]);
-            }
+            $product->productTypes()->attach($product->id, ['product_type_id' => 1]);
+            $product->productTypes()->attach($product->id, ['product_type_id' => 2]);
+            $product->productTypes()->attach($product->id, ['product_type_id' => 3]);
+        }
 
-            for ($i = 1; $i <= 3; $i++) {
-                $product->productTypes()->attach($product->id, ['product_type_id' => $i]);
-            }
+        {
+            $product = Product::create([
+                'name' => 'TestDames',
+                'image_path' => '/images/products/placeholder.png',
+            ]);
+            $product->productSizes()->attach($product->id, ['product_size_id' => 2, 'price' => 23.45]);
+            $product->productSizes()->attach($product->id, ['product_size_id' => 3, 'price' => 34.56]);
+
+            $product->productTypes()->attach($product->id, ['product_type_id' => 1]);
+            $product->productTypes()->attach($product->id, ['product_type_id' => 2]);
+            $product->productTypes()->attach($product->id, ['product_type_id' => 3]);
+        }
+        {
+            $product = Product::create([
+                'name' => 'TestHeren',
+                'image_path' => '/images/products/placeholder.png',
+            ]);
+            $product->productSizes()->attach($product->id, ['product_size_id' => 1, 'price' => 12.34]);
+            $product->productSizes()->attach($product->id, ['product_size_id' => 3, 'price' => 34.56]);
+
+            $product->productTypes()->attach($product->id, ['product_type_id' => 1]);
+            $product->productTypes()->attach($product->id, ['product_type_id' => 2]);
+            $product->productTypes()->attach($product->id, ['product_type_id' => 3]);
+        }
+        {
+            $product = Product::create([
+                'name' => 'TestUnisex',
+                'image_path' => '/images/products/placeholder.png',
+            ]);
+            $product->productSizes()->attach($product->id, ['product_size_id' => 1, 'price' => 12.34]);
+            $product->productSizes()->attach($product->id, ['product_size_id' => 2, 'price' => 23.45]);
+
+            $product->productTypes()->attach($product->id, ['product_type_id' => 1]);
+            $product->productTypes()->attach($product->id, ['product_type_id' => 2]);
+            $product->productTypes()->attach($product->id, ['product_type_id' => 3]);
+        }
+        {
+            $product = Product::create([
+                'name' => 'TestSingleSize',
+                'image_path' => '/images/products/placeholder.png',
+            ]);
+            $product->productSizes()->attach($product->id, ['product_size_id' => 2, 'price' => 23.45]);
+
+            $product->productTypes()->attach($product->id, ['product_type_id' => 1]);
+            $product->productTypes()->attach($product->id, ['product_type_id' => 2]);
+            $product->productTypes()->attach($product->id, ['product_type_id' => 3]);
         }
     }
+
 }
