b628aaba10ad3f2d59cb8203d287c72ee7e1c2d4
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-05-17 10:44:53 +0200

    refactor(roles): rows don't shift in height
    Rows used to shift in height and the plus sign wasn't centered

--- resources/views/admin/accounts.blade.php
+++ resources/views/admin/accounts.blade.php
@@ -108,10 +108,10 @@
                                         @endforeach
                                     </select>
                                 </div>
-                                <div id="addRoleButton" class="text-blue-600 h-10">
-                                    <div class="hs-tooltip inline-block">
-                                        <button type="button" class="hs-tooltip-toggle">
-                                            <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none"
+                                <div id="addRoleButton" class="text-blue-600 h-[46px]">
+                                    <div class="hs-tooltip inline-block h-[46px]">
+                                        <button type="button" class="hs-tooltip-toggle items-center">
+                                            <svg width="46px" height="46px" viewBox="0 0 24 24" fill="none"
                                                  xmlns="http://www.w3.org/2000/svg">
                                                 <path fill-rule="evenodd" clip-rule="evenodd"
                                                       d="M12 2C12.5523 2 13 2.44772 13 3V11H21C21.5523 11 22 11.4477 22 12C22 12.5523 21.5523 13 21 13H13V21C13 21.5523 12.5523 22 12 22C11.4477 22 11 21.5523 11 21V13H3C2.44772 13 2 12.5523 2 12C2 11.4477 2.44772 11 3 11H11V3C11 2.44772 11.4477 2 12 2Z"
