23890317916d471111ef4ef9bfbfffa55c9648fe
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-05-18 09:23:13 +0200

    feat(roles): added styling for subrole selection

--- resources/views/admin/accounts.blade.php
+++ resources/views/admin/accounts.blade.php
@@ -84,7 +84,27 @@
                             <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
                                 {{ $account['email'] }}
                             </td>
-                            <td class="flex items-center justify-end px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+                            <td class="flex items-center gap-4 justify-end px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+                                <div class="relative">
+                                    <select id="subroleSelect{{ $account->id }}" class="peer p-4 pe-9 block w-[150px] border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-slate-700 dark:text-gray-400 dark:focus:ring-neutral-600
+                                        focus:pt-6
+                                        focus:pb-2
+                                        [&:not(:placeholder-shown)]:pt-6
+                                        [&:not(:placeholder-shown)]:pb-2
+                                        autofill:pt-6
+                                        autofill:pb-2">
+                                        <option>A</option>
+                                        <option>B</option>
+                                        <option>C</option>
+                                    </select>
+                                    <label for="subroleSelect{{ $account->id }}" class="absolute top-0 start-0 p-4 h-full truncate pointer-events-none transition ease-in-out duration-100 border border-transparent peer-disabled:opacity-50 peer-disabled:pointer-events-none
+                                        peer-focus:text-xs
+                                        peer-focus:-translate-y-1.5
+                                        peer-focus:text-gray-500 dark:peer-focus:text-neutral-500
+                                        peer-[:not(:placeholder-shown)]:text-xs
+                                        peer-[:not(:placeholder-shown)]:-translate-y-1.5
+                                        peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-gray-500 dark:text-gray-500">Bevers</label>
+                                </div>
                                 <div id="selectRoleContainer" class="flex flex-row hidden">
                                     <div id="selectRole-wrapper" class="relative w-[250px]"
                                          data-account-email="{{ $account->email }}"
