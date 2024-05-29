ad93831afbca8a9c243dae52b210ebc7e45260d4
VindSent <vincentvanhintum@gmail.com>
2024-05-29 22:52:31 +0200

    refactor(orderline): better pink and purple

--- database/factories/OrderLineFactory.php
+++ database/factories/OrderLineFactory.php
@@ -26,14 +26,15 @@ class OrderLineFactory extends Factory
     {
         $product = Product::query()->inRandomOrder()->first() ?? Product::factory()->create();
 
-        $files = File::files(public_path('/images/products/'));
+        $prefix = 'images/products/';
+        $files = File::files(public_path($prefix));
         $randomFile = $files[array_rand($files)];
 
         return [
             'product_id' => $product->id,
             'product_price' => $this->faker->randomFloat(2, 1, 100),
             'product_size' => $this->faker->randomElement(['S', 'M', 'L', 'XL']),
-            'product_image_path' => 'images/products/' . $randomFile->getFilename(),
+            'product_image_path' => $prefix . $randomFile->getFilename(),
             'amount' => $this->faker->numberBetween(1, 10),
             'product_type_id' => ProductProductType::where('product_id', $product->id)->first()->product_type_id,
         ];
