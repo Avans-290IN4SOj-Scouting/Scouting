e8357cb2a1e91a00fa9467c2335c366616489908
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-05-30 21:23:47 +0200

    refactor(roles): removal comment

--- resources/views/admin/accounts.blade.php
+++ resources/views/admin/accounts.blade.php
@@ -121,7 +121,6 @@
                                                       d="M12 2C12.5523 2 13 2.44772 13 3V11H21C21.5523 11 22 11.4477 22 12C22 12.5523 21.5523 13 21 13H13V21C13 21.5523 12.5523 22 12 22C11.4477 22 11 21.5523 11 21V13H3C2.44772 13 2 12.5523 2 12C2 11.4477 2.44772 11 3 11H11V3C11 2.44772 11.4477 2 12 2Z"
                                                       fill="currentColor"/>
                                             </svg>
-                                            <!-- TODO: add localisation when done -->
                                             <span
                                                 class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded shadow-sm dark:bg-neutral-700"
                                                 role="tooltip">{{ __('manage-accounts/accounts.add-role') }}</span>
