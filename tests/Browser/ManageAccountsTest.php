528ec99160ecaf2104881fc757d814a8d408f420
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-06-10 20:42:16 +0200

    feat(tests): new modal appears test

--- tests/Browser/ManageAccountsTest.php
+++ tests/Browser/ManageAccountsTest.php
@@ -81,24 +81,30 @@ class ManageAccountsTest extends DuskTestCase
      */
     public function testModalAppearsAfterRoleChange()
     {
-        $admin = User::factory()->create(['email' => 'admin@test.com'])->assignRole('admin');
+        $admin = User::factory()->create(['email' => 'role.dropdown.test']);
+        $admin->assignRole('admin');
 
-        $testUser = User::factory()->create(['email' => 'aaa@a.a'])->assignRole('user');
+        $userToEdit = User::find(1);
+        $groupToSelect = Group::find(1);
 
-        $this->browse(function (Browser $browser) use ($admin, $testUser) {
+        $this->browse(function (Browser $browser) use ($admin, $userToEdit, $groupToSelect) {
             $browser->loginAs($admin)
-                ->visit(route('manage.accounts.index'))
-                ->clickLink(__('manage-accounts/accounts.email'))
-                ->click('[data-account-email="' . $testUser->email . '"]')
-                ->waitFor('#selectRole-div')
-                ->click(__('[data-value="admin"]'))
-                ->click('[data-account-email="' . $testUser->email . '"]')
+                ->visitRoute('manage.accounts.index')
+                ->pause(5000)
+                ->whenAvailable('#roleContainer' . $userToEdit->id, function ($container) {
+                    $container->click('#addRoleButton');
+                })
+                ->waitFor('#selectRole' . $userToEdit->id)
+                ->assertVisible('#selectRole' . $userToEdit->id)
+                ->select('#selectRole' . $userToEdit->id, $groupToSelect->name)
+                ->pause(5000)
+                ->within('#roleContainer' . $userToEdit->id, function ($container) use ($groupToSelect) {
+                    $container->waitFor('#subroleSelect' . $groupToSelect->name . '1')
+                        ->assertVisible('#subroleSelect' . $groupToSelect->name . '1');
+                })
+                ->waitFor('#saveBtn')
                 ->click('#saveBtn')
-                ->waitFor('.confirmModal', 10)
-                ->assertVisible('.confirmModal')
-                ->assertSee(__('manage-accounts/accounts.modal_warning_title'))
-                ->assertSee(__('manage-accounts/accounts.confirm_button'))
-                ->screenshot('manage-accounts/modal');
+                ->assertVisible('#confirmModal');
         });
     }
 
