a1f208639fbf87851f4e04b1996672f77bff8ebe
Delite Borst <58737124+Auwburn@users.noreply.github.com>
2024-06-10 21:04:21 +0200

    refactor(tests): removal redundant test
    + name change

--- tests/Browser/ManageAccountsTest.php
+++ tests/Browser/ManageAccountsTest.php
@@ -29,32 +29,6 @@ class ManageAccountsTest extends DuskTestCase
         });
     }
 
-    public function testAddRoleSelect()
-    {
-        $admin = User::factory()->create(['email' => 'role.dropdown.test']);
-        $admin->assignRole('admin');
-
-        $userToEdit = User::find(1);
-        $groupToSelect = Group::find(1);
-
-        $this->browse(function (Browser $browser) use ($admin, $userToEdit, $groupToSelect) {
-            $browser->loginAs($admin)
-                ->visitRoute('manage.accounts.index')
-                ->pause(5000)
-                ->whenAvailable('#roleContainer' . $userToEdit->id, function ($container) {
-                    $container->click('#addRoleButton');
-                })
-                ->waitFor('#selectRole' . $userToEdit->id)
-                ->assertVisible('#selectRole' . $userToEdit->id)
-                ->select('#selectRole' . $userToEdit->id, $groupToSelect->name)
-                ->pause(5000)
-                ->within('#roleContainer' . $userToEdit->id, function ($container) use ($groupToSelect) {
-                    $container->waitFor('#subroleSelect' . $groupToSelect->name . '1')
-                        ->assertVisible('#subroleSelect' . $groupToSelect->name . '1');
-                });
-        });
-    }
-
     public function testExistingRoleAddsRoleSelect() {
         $admin = User::factory()->create(['email' => 'role.existing.select']);
         $admin->assignRole('admin');
@@ -74,12 +48,8 @@ class ManageAccountsTest extends DuskTestCase
                 });
         });
     }
-
-    /**
-     * @group manage
-     * @return void
-     */
-    public function testModalAppearsAfterRoleChange()
+    
+    public function testAddRoleAndShowModal()
     {
         $admin = User::factory()->create(['email' => 'modal.displayed']);
         $admin->assignRole('admin');
